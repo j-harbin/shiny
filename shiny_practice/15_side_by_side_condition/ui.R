9ba3cee6d67c9dd4f03e7aefbda9117a15d66d6a
Jaimie Harbin <jm810136@dal.ca>
2021-02-01 12:56:34 -0400

    .

--- shiny_practice/15_side_by_side_condition/ui.R
+++ shiny_practice/15_side_by_side_condition/ui.R
@@ -5,6 +5,5 @@ ui <- fluidPage(
         shiny::sliderInput("Csymbol", "Symbol Type", min=0, max=25, value=21, step=1)),
                           shiny::conditionalPanel("input.Csymbol== 21",
                                                      shiny::div(style="display: inline-block;vertical-align:top; width: 8em;",
-
-    shiny::sliderInput("Csize", "Symbol Size", min=0, max=1, value=0.5, step=0.05)))
+                                                                shiny::selectInput("Cborder", "Border Colour", choices=c("1","2", "3", "4", "5", "6", "7", "8"), selected="1")))
 ))
