ed47721e97bc59b9e2e1b56c46acb80da2af5f0d
Jaimie Harbin <jm810136@dal.ca>
2021-02-05 10:47:58 -0400

    add more QC widgets

--- shiny_practice/16_qcApp/server.R
+++ shiny_practice/16_qcApp/server.R
@@ -1,23 +1,40 @@
 server <- shinyServer(function(input,output){
 
 
-output$UIID <- shiny::renderUI({
-        if (input$tabselected %in% c(2)) {
-            shiny::mainPanel(shiny::textInput("ID", "Float ID", value="", width="11em"))
-        }
-    })
+                          output$UIfocus <- shiny::renderUI({
+                              if (input$tabselected %in% c(2)) {
+                                  shiny::fluidRow(
+                                                  shiny::column(2, shiny::textInput("ID", "Float ID", value="", width="11em")),
+                                                  shiny::column(2, shiny::textInput("cycle", "Cycle", value="", width="11em")))
+                              }
+                          })
 
-output$UIcycle <- shiny::renderUI({
-        if (input$tabselected %in% c(2)) {
-            shiny::mainPanel(shiny::textInput("cycle", "Cycle", value="", width="11em"))
-        }
-    })
+                          output$UIwidget <- shiny::renderUI({
+                              if (input$tabselected %in% c(1, 2)) {
+                                  shiny::fluidRow(shiny::column(2,shiny::span(shiny::HTML(paste("<b style=\"color:black; margin-left:1em;\">  ","qcApp 0.1","</b>")))),
+                                                  shiny::column(2, shiny::actionButton("help", "Help")),
+                                                  shiny::column(2, shiny::actionButton("code", "Code")))}})
 
-output$UIwidget <- shiny::renderUI({
-        if (input$tabselected %in% c(1, 2)) {
-                             shiny::fluidRow(shiny::column(2,shiny::span(shiny::HTML(paste("<b style=\"color:blue; margin-left:1em;\">  ","qcApp 0.1","</b>")))),
-                                             shiny::column(2, shiny::actionButton("help", "Help")),
-                                             shiny::column(2, shiny::actionButton("code", "Code")))}})
+                          output$UIsingleQC <- shiny::renderUI({
+                              if (input$tabselected %in% c(2)) {
+                                  shiny::checkboxGroupInput("qc",
+                                                            label="Single float QC",
+                                                            choiceNames=list(shiny::tags$span("applyQC", style="color:black;"),
+                                                                             shiny::tags$span("showQCTests", style="color:black;"),
+                                                                             shiny::tags$span("Diagnostics", style="color:black;")),
+                                                            choiceValues=list("applyQC","showQCTests", "diagnostics"),
+                                                            inline=TRUE)
+                              }
+                          })
 
+                          output$UIplot <- shiny::renderUI({
+                              ##FIXME: Add glider options to both plot types
+                              if (input$tabselected %in% c(1)) {
+
+                                  shiny::fluidRow(shiny::column(3, shiny::selectInput("type", "Plot Profile", choices=c("Salinity", "Temperature", "Oxygen"), selected="Temperature")),
+
+                                  shiny::column(3, shiny::selectInput("colorcode", "Color-code by", choices=c("Temperature", "Latitude", "Oxygen"), selected="Temperature")))}
+
+                          })
 
 })
--- shiny_practice/16_qcApp/ui.R
+++ shiny_practice/16_qcApp/ui.R
@@ -2,15 +2,17 @@ library(shiny)
 
 ui <- fluidPage(
 
- shiny::tabsetPanel(type="tab",
-                                                                 shiny::tabPanel("Main", value=1),
-                                                                 shiny::tabPanel("Single Float", value=2),
-                  id="tabselected"),
-                
+                shiny::tabsetPanel(type="tab",
+                                   shiny::tabPanel("Main", value=1),
+                                   shiny::tabPanel("Single Float", value=2),
+                                   id="tabselected"),
                 shiny::fluidRow(
                                 shiny::uiOutput(outputId="UIwidget")),
 
-                          shiny::fluidRow(
-                                          shiny::uiOutput(outputId="UIID"),
-                                          shiny::uiOutput(outputId="UIcycle"))
- )
+                shiny::fluidRow(
+                                shiny::uiOutput(outputId="UIfocus")),
+
+                shiny::fluidRow(shiny::uiOutput(outputId="UIsingleQC")),
+                shiny::fluidRow(shiny::uiOutput(outputId="UIplot"))
+
+)
