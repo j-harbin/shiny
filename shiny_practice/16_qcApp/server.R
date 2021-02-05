9a03a34858d196636263c40be7475342e84315ae
Jaimie Harbin <jm810136@dal.ca>
2021-02-05 11:19:52 -0400

    update qcApp()

--- shiny_practice/16_qcApp/server.R
+++ shiny_practice/16_qcApp/server.R
@@ -28,12 +28,45 @@ server <- shinyServer(function(input,output){
                           })
 
                           output$UIplot <- shiny::renderUI({
-                              ##FIXME: Add glider options to both plot types
+                              ## NOTE: The type and colorBy are the same as oceanglider
                               if (input$tabselected %in% c(1)) {
 
-                                  shiny::fluidRow(shiny::column(3, shiny::selectInput("type", "Plot Profile", choices=c("Salinity", "Temperature", "Oxygen"), selected="Temperature")),
+                                  shiny::fluidRow(
+                                                  shiny::column(3, shiny::selectInput("type", "Plot Type",choices=c("TS <T>"="TS",
+                          "C(t)"="conductivity time-series",
+                          "p(t) <P>"="pressure time-series",
+                          "S(t) <S>"="salinity time-series",
+                          "spiciness(t)"="spiciness time-series",
+                          "T(t)"="temperature time-series",
+                          "tSincePowerOn(t)"="tSincePowerOn time-series",
+                          "C(p)"="conductivity profile",
+                          "density(p)"="density profile",
+                          "S(p)"="salinity profile",
+                          "spiciness(p)"="spiciness profile",
+                          "T(p)"="temperature profile",
+                          "hist(C)"="conductivity histogram",
+                          "hist(p)"="pressure histogram",
+                          "hist(S)"="salinity histogram",
+                          "hist(T)"="temperature histogram"),
+                selected="pressure time-series")),
 
-                                  shiny::column(3, shiny::selectInput("colorcode", "Color-code by", choices=c("Temperature", "Latitude", "Oxygen"), selected="Temperature")))}
+                                  shiny::column(3, shiny::selectInput(inputId="colorBy",
+                label="Color by",
+                choices=c("distance"="distance",
+                          "latitude"="latitude",
+                          "longitude"="longitude",
+                          "p"="pressure",
+                          "T"="temperature",
+                          "S"="salinity",
+                          "density"="sigma0",
+                          "N2"="N2",
+                          "instability"="instability",
+                          "oxygen",
+                          "navState"="navState",
+                          "tSincePowerOn"="tSincePowerOn",
+                          "spiciness"="spiciness0",
+                          "(none)"="(none)"),
+                selected="(none)")))}
 
                           })
 
