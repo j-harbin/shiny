fe6deb6ae6f8d7022ed3f385d4f11798922b7a5f
Jaimie Harbin <jm810136@dal.ca>
2021-06-24 09:20:42 -0300

    change to variable_lookup

--- shiny_practice/19_azmp_try/app.R
+++ shiny_practice/19_azmp_try/app.R
@@ -1,9 +1,33 @@
+library(shiny)
+library(azmpdata)
+library(leaflet)
+
 
 #FIXME: Finish overallHelp when parameters is coded in
 
 overallHelp <- "This GUI has two tabs,  <b> Map </b> and <b> Parameters </b>. <br><br> The <b>Map</b> tab can be used to visualize where each station, section, and area is located for the Atlantic Zone Monitoring Program (AZMP) data. Stations associated with the azmpdata dataframes are displayed in black. If the user single clicks on the station, the station name, depth, and associated dataset within the azmpdata package (if applicable) will appear. If the user single clicks on an area of station, the section or area name will appear with the associated dataset within azmpdata package if applicable. <br><br> <b> Note </b> on the Map, NAFO stands for 'Northwest Atlantic Fisheries Organization' and 'SS' stands for Scotian Shelf. <br><br> On the  <b> Parameters </b> tab,"
 
-shinyServer(function(input, output){
+
+
+
+shinyUI <- fluidPage(
+
+                  titlePanel("GUI for azmpdata package."),
+                  sidebarLayout(
+                                sidebarPanel(
+                                             conditionalPanel(condition="input.tabselected==1", shiny::actionButton("help", "Help"),
+                                                              shiny::actionButton("code","Code")),
+                                             conditionalPanel(condition="input.tabselected==2", shiny::radioButtons("type", "Select your type of parameter", choices=c("Physical", "Chemical", "Biological"), selected=c("Physical")))),
+                                                          mainPanel(
+tabsetPanel(type="tab",
+            tabPanel("Map", value=1, leaflet::leafletOutput("map")),
+            tabPanel("Parameters", value=2, plotOutput("temperature1")),
+id = "tabselected")
+
+                                                          )))
+
+
+shinyServer <- shiny::shinyServer(function(input, output){
                 output$map <- renderLeaflet({
                     azmpdata::azmpmap()
                    })
@@ -22,4 +46,13 @@ shinyServer(function(input, output){
                                         shiny::showModal(shiny::modalDialog(shiny::HTML(msg), title="Using this application", size="l"))
                                     })
 })
+
+
+variable_lookup <- function()
+{
+    if (!requireNamespace("shiny", quietly=TRUE))
+        stop("must install.packages(\"shiny\") for this to work")
+    print(shiny::shinyApp(ui=shinyUI, server=shinyServer))
+}
+
 
--- shiny_practice/19_azmp_try/ui.R
+++ shiny_practice/19_azmp_try/ui.R
@@ -8,7 +8,9 @@ library(leaflet)
 overallHelp <- "This GUI has two tabs,  <b> Map </b> and <b> Parameters </b>. <br><br> The <b>Map</b> tab can be used to visualize where each station, section, and area is located for the Atlantic Zone Monitoring Program (AZMP) data. Stations associated with the azmpdata dataframes are displayed in black. If the user single clicks on the station, the station name, depth, and associated dataset within the azmpdata package (if applicable) will appear. If the user single clicks on an area of station, the section or area name will appear with the associated dataset within azmpdata package if applicable. <br><br> <b> Note </b> on the Map, NAFO stands for 'Northwest Atlantic Fisheries Organization' and 'SS' stands for Scotian Shelf. <br><br> On the  <b> Parameters </b> tab,"
 
 
-shinyUI(fluidPage(
+
+
+shinyUI <- fluidPage(
 
                   titlePanel("GUI for azmpdata package."),
                   sidebarLayout(
@@ -22,4 +24,35 @@ tabsetPanel(type="tab",
             tabPanel("Parameters", value=2, plotOutput("temperature1")),
 id = "tabselected")
 
-                                                          ))))
+                                                          )))
+
+
+shinyServer <- shiny::shinyServer(function(input, output){
+                output$map <- renderLeaflet({
+                    azmpdata::azmpmap()
+                   })
+
+                output$temperature1 <- renderPlot({
+                    output$temperature1 <- renderPlot({
+                            plot(Discrete_Annual_Broadscale$year, Discrete_Annual_Broadscale$sea_temperature,xlab= "Year", ylab="Broadscale Annual Sea Temperature (C)", pch=20, type='o', main="Discrete Annual Broadscale Temperature Analysis")
+                            regression <- lm(Discrete_Annual_Broadscale$sea_temperature ~ Discrete_Annual_Broadscale$year)
+                            abline(regression, h=50, col='red')
+                    })
+                })
+
+                shiny::observeEvent(input$help,
+                                    {
+                                        msg <- shiny::HTML(overallHelp)
+                                        shiny::showModal(shiny::modalDialog(shiny::HTML(msg), title="Using this application", size="l"))
+                                    })
+})
+
+
+variable_lookup <- function()
+{
+    if (!requireNamespace("shiny", quietly=TRUE))
+        stop("must install.packages(\"shiny\") for this to work")
+    print(shiny::shinyApp(ui=shinyUI, server=shinyServer))
+}
+
+
