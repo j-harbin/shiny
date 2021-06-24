70180dde11123fbbd45ebb9cd6ffa9eef375a0f6
Jaimie Harbin <jm810136@dal.ca>
2021-06-24 08:53:36 -0300

    add code and help button

--- shiny_practice/19_azmp_try/ui.R
+++ shiny_practice/19_azmp_try/ui.R
@@ -9,6 +9,8 @@ shinyUI(fluidPage(
                   titlePanel("GUI for azmpdata package."),
                   sidebarLayout(
                                 sidebarPanel(
+                                             conditionalPanel(condition="input.tabselected==1", shiny::actionButton("help", "Help"),
+                                                              shiny::actionButton("code","Code")),
                                              conditionalPanel(condition="input.tabselected==2", shiny::radioButtons("type", "Select your type of parameter", choices=c("Physical", "Chemical", "Biological"), selected=c("Physical")))),
                                                           mainPanel(
 tabsetPanel(type="tab",
